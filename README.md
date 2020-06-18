befaf90fd28c74dc552e0654d8a29ca6143ea2cb
Janos Wortmann <jwortmann@outlook.com>
2020-06-18 18:57:53 +0200

    Add files

---
+++ README.md
@@ -0,0 +1,56 @@
+# LSP-julia
+
+[![License](https://img.shields.io/github/license/jwortmann/LSP-julia)](https://github.com/jwortmann/LSP-julia/blob/master/LICENSE)
+
+A plugin for the [LSP](https://packagecontrol.io/packages/LSP) client in Sublime Text with support for the [LanguageServer.jl](https://github.com/julia-vscode/LanguageServer.jl) Julia language server.
+
+IMPORTANT: This plugin is in an early stage and under development, so it might not work flawlessly yet and there could be some breaking changes in the future.
+
+## Installation
+
+* Install [LanguageServer.jl](https://github.com/julia-vscode/LanguageServer.jl) from the Julia REPL (ensure to use version v3.0.0 or higher!):
+
+```
+julia> ]
+pgk> add LanguageServer
+```
+
+* Install the [Julia](https://packagecontrol.io/packages/Julia) package from Package Control for syntax highlighting
+* Install the [LSP](https://packagecontrol.io/packages/LSP) client from Package Control
+* Install this LSP-julia package from Package Control.
+  For now, you have to use `Package Control: Add Repository` and paste this repository URL to make it available in Package Control.
+  I probably will wait until there is a public release of ST4 and then update this plugin to use the new LSP API, before considering to add it to the default Package Control channel.
+* Optionally install the [Terminus](https://packagecontrol.io/packages/Terminus) package from Package Control for a better Julia REPL integration and additional features.
+
+## Features
+
+Most of the standard LSP features like autocompletion, documentation on hover, or goto definition are supported by the Julia language server.
+
+LSP-julia provides additional commands which are available from the command palette:
+
+| Command label | Command name | Key binding | Description |
+| ------------- | ------------ | ----------- | ----------- |
+| LSP-julia: Change Environment | `julia_activate_environment` | none | Choose the Julia project environment you are working in. Its packages are used by the language server to provide autocomplete suggestions. The server will take a while for indexing packages after running this command. |
+| LSP-julia: Select Code Block | `julia_select_code_block` | none | Select the function or code block at the current cursor position. For multiple active cursors, only the topmost cursor position is taken into account. |
+| LSP-julia: Run Code Block | `julia_run_code_block` | <kbd>Ctrl</kbd>+<kbd>Enter</kbd> | Run the function or code block at the current cursor position in a Julia REPL. This command is only available if you have the [Terminus](https://packagecontrol.io/packages/Terminus) package installed. |
+| LSP-julia: Expand Inline Function | See [LSP-julia.sublime-commands](LSP-julia.sublime-commands) | !! Needs  a bugfix in LanguageServer.jl or a small adjustment in the LSP package to work. Might crash the server if not run with curser inside an inline function. !! |
+
+If you want to adjust or create additional key bindings for the commands, just copy the examples from the `.sublime-keymap` file for your OS into the corresponding file in your `Packages/User` folder.
+
+## Miscellaneous
+
+The startup time for the Julia language server can be noticeably reduced by precompiling the LanguageServer.jl package via [PackageCompiler.jl](https://github.com/JuliaLang/PackageCompiler.jl) into a system image file:
+```
+julia> using PackageCompiler
+pkg> activate .
+pkg> add LanguageServer
+julia> create_sysimage(:LanguageServer; sysimage_path="LanguageServer.so")
+```
+Then add the starting option `"--sysimage", "path/to/LanguageServer.so"` into `"command"` in your user settings file.
+
+## Known issues and workarounds
+
+* The most current version (v3.1.0) of the language server crashes at start if the `"settings"` object for server related settings is not empty, see [LanguageServer.jl#754](https://github.com/julia-vscode/LanguageServer.jl/issues/754).
+  As a workaround for now, leave `"settings"` empty, the server will use default values for linting and formatting options then.
+* LSP leaves orphaned Julia processes for the language server, see [LSP#410](https://github.com/sublimelsp/LSP/issues/410) and [LSP#869](https://github.com/sublimelsp/LSP/issues/869).
+  There is a workaround for Linux/macOS to start the server via a [Bash script](https://github.com/julia-vscode/LanguageServer.jl/blob/master/contrib/languageserver.sh) which periodically checks for and kills orphaned language server processes, but that doesn't allow to specify a path to the active Julia environment in its starting arguments.
---
+++ plugin.py
@@ -0,0 +1,248 @@
+import os
+import re
+import importlib
+import sublime
+import sublime_plugin
+
+from LSP.plugin.core.handlers import LanguageHandler
+from LSP.plugin.core.settings import read_client_config
+from LSP.plugin.core.protocol import Request, Notification, Point
+from LSP.plugin.core.registry import LspTextCommand
+from LSP.plugin.core.views import text_document_position_params, point_to_offset
+from LSP.plugin.execute_command import LspExecuteCommand
+
+from .utils import load_settings
+
+
+SETTINGS_FILE = "LSP-julia.sublime-settings"
+
+
+def get_active_environment() -> tuple:
+    settings = sublime.load_settings(SETTINGS_FILE)
+    command = settings.get("command", [])
+    command_str = command[-1]
+    regex = re.compile("env_path=raw\".+\";")
+    m = regex.findall(command_str)
+    if len(m) != 1:
+        return ""
+    env_path = m[0][13:-2]
+    env_name = os.path.basename(env_path)
+    return env_name, env_path
+
+
+def is_project_folder(env_path: str) -> bool:
+    return os.path.isfile(os.path.join(env_path, "Project.toml")) or os.path.isfile(os.path.join(env_path, "JuliaProject.toml"))
+
+
+def update_environment_settings(env_path: str) -> None:
+    settings = sublime.load_settings(SETTINGS_FILE)
+    command = settings.get("command")
+    if command:
+        command[-1] = "using LanguageServer, LanguageServer.SymbolServer; env_path=raw\"{}\"; depot_path=first(Base.DEPOT_PATH); server=LanguageServer.LanguageServerInstance(stdin,stdout,env_path,depot_path); run(server)".format(env_path)
+        settings.set("command", command)
+        sublime.save_settings(SETTINGS_FILE)
+
+
+def update_environment_status(window: sublime.Window, env_name: str) -> None:
+    for view in window.views():
+        if view.match_selector(0, "source.julia"):
+            view.set_status("lsp_clients_julia", env_name)
+
+
+class JuliaFileListener(sublime_plugin.EventListener):
+    def on_load(self, view: sublime.View) -> None:
+        if not view.match_selector(0, "source.julia"):
+            return
+        settings = sublime.load_settings(SETTINGS_FILE)
+        if not settings.get("enabled", True):
+            return
+        if not settings.get("show_environment_status"):
+            return
+        env_name = get_active_environment()[0]
+        if env_name:
+            view.set_status("lsp_clients_julia", env_name)
+
+
+class LspJuliaPlugin(LanguageHandler):
+    _window = None
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    @property
+    def name(self) -> str:
+        return "julia"
+
+    @property
+    def config(self):
+        settings = sublime.load_settings(SETTINGS_FILE)
+        julia_executable_path = settings.get("julia_executable_path")
+        command = settings.get("command")
+        command[0] = julia_executable_path or "julia"
+        settings.set("command", command)
+        sublime.save_settings(SETTINGS_FILE)
+        settings = load_settings(SETTINGS_FILE)
+        return read_client_config(self.name, settings)
+
+    def on_start(self, window) -> bool:
+        self._window = window
+        return True
+
+    def on_initialized(self, client) -> None:
+        settings = sublime.load_settings(SETTINGS_FILE)
+        if settings.get("auto_change_environment"):
+            current_env_path = get_active_environment()[1]
+            for folder in self._window.folders():
+                if is_project_folder(folder):
+                    if folder != current_env_path:
+                        client.send_notification(Notification("julia/activateenvironment", folder))
+                        update_environment_settings(folder)
+                    break
+        if settings.get("show_environment_status"):
+            env_name = get_active_environment()[0]
+            if env_name:
+                update_environment_status(self._window, env_name)
+
+
+class JuliaActivateEnvironmentCommand(LspTextCommand):
+    def is_enabled(self):
+        return self.view.match_selector(0, "source.julia") and self.client_with_capability(None) is not None
+
+    def run(self, edit, env_path):
+        if env_path[-1] in {"/", "\\"}:
+            env_path = env_path[0:-1]
+
+        # send julia/activateenvironment notification
+        client = self.client_with_capability(None)
+        client.send_notification(Notification("julia/activateenvironment", env_path))
+
+        # update settings
+        update_environment_settings(env_path)
+
+        # update status bar
+        settings = load_settings(SETTINGS_FILE)
+        if settings.get("show_environment_status"):
+            env_name = os.path.basename(env_path)
+            update_environment_status(self.view.window(), env_name)
+
+    def input(self, args):
+        return EnvPathInputHandler(self.view)
+
+
+class EnvPathInputHandler(sublime_plugin.ListInputHandler):
+    def __init__(self, view):
+        self.view = view
+
+    def list_items(self):
+        # add folders in .julia/environments
+        julia_env_home = os.path.expanduser(os.path.join("~", ".julia", "environments"))
+        julia_env_names = [env for env in os.listdir(julia_env_home) if os.path.isdir(os.path.join(julia_env_home, env))]
+        julia_env_paths = [os.path.join(julia_env_home, env) for env in julia_env_names]
+        julia_env = [list(env) for env in zip(julia_env_names, julia_env_paths)]
+        # check and add project folders
+        for folder_path in reversed(self.view.window().folders()):
+            if folder_path not in julia_env_paths and is_project_folder(folder_path):
+                folder_name = os.path.basename(folder_path)
+                julia_env.insert(0, [folder_name, folder_path])
+        return julia_env
+
+    def placeholder(self):
+        return "Select Julia project folder"
+
+    def preview(self, value):
+        return sublime.Html("<i>{}</i>".format(value)) if value else None
+
+    def validate(self, value):
+        return value is not None
+
+
+class JuliaSelectCodeBlockCommand(LspTextCommand):
+    def is_enabled(self):
+        return self.view.match_selector(0, "source.julia") and self.client_with_capability(None) is not None
+
+    def run(self, edit):
+        # send julia/getCurrentBlockRange request
+        params = text_document_position_params(self.view, self.view.sel()[0].b)
+        client = self.client_with_capability(None)
+        client.send_request(Request("julia/getCurrentBlockRange", params), self.handle_response)
+
+    def handle_response(self, response):
+        a = point_to_offset(Point.from_lsp(response[0]), self.view)
+        b = point_to_offset(Point.from_lsp(response[1]), self.view)
+        self.view.sel().clear()
+        self.view.run_command("lsp_selection_add", {"regions": [(a, b)]})
+        self.view.show_at_center(sublime.Region(a, b))
+
+
+class JuliaRunCodeBlockCommand(LspTextCommand):
+    def is_enabled(self):
+        if not self.view.match_selector(0, "source.julia"):
+            return False
+        if not importlib.find_loader("Terminus"):
+            return False
+        if not self.client_with_capability(None):
+            return False
+        return True
+
+    def run(self, edit):
+        if not self.is_enabled():
+            return
+        # don't execute if curser is at end of file
+        if self.view.sel()[0].b == self.view.size():
+            return
+        # ensure that Terminus output panel for Julia REPL is available
+        if not self.view.window().find_output_panel("Julia REPL"):
+            settings = sublime.load_settings(SETTINGS_FILE)
+            self.view.window().run_command("terminus_open", {
+                "cmd": settings.get("julia_executable_path") or "julia",
+                "cwd": os.path.dirname(self.view.file_name()),
+                "panel_name": "Julia REPL",
+                "focus": False,
+                "tag": "lsp_julia_repl"
+            })
+        # send julia/getCurrentBlockRange request
+        params = text_document_position_params(self.view, self.view.sel()[0].b)
+        client = self.client_with_capability(None)
+        client.send_request(Request("julia/getCurrentBlockRange", params), self.handle_response)
+
+    def handle_response(self, response):
+        a = point_to_offset(Point.from_lsp(response[0]), self.view)
+        b = point_to_offset(Point.from_lsp(response[1]), self.view)
+        code_block = self.view.substr(sublime.Region(a, b)) + "\n"
+        # move cursor to next code block
+        self.view.sel().clear()
+        while self.view.substr(b) in {" ", "\t", "\n"} or self.view.match_selector(b, "comment"):
+            b += 1
+        self.view.run_command("lsp_selection_add", {"regions": [(b, b)]})
+        self.view.show_at_center(b)
+        # send code block to Terminus Julia REPL
+        self.view.window().run_command("terminus_send_string", {"string": code_block, "tag": "lsp_julia_repl"})
+
+
+class JuliaOpenReplCommand(LspTextCommand):
+    def is_enabled(self):
+        if not self.view.match_selector(0, "source.julia"):
+            return False
+        if not importlib.find_loader("Terminus"):
+            return False
+        return True
+
+    def run(self, edit):
+        if not self.is_enabled():
+            return
+        repl_view = self.view.window().find_output_panel("Julia REPL")
+        if not repl_view:
+            settings = sublime.load_settings(SETTINGS_FILE)
+            self.view.window().run_command("terminus_open", {
+                "cmd": settings.get("julia_executable_path") or "julia",
+                "cwd": os.path.dirname(self.view.file_name()),
+                "panel_name": "Julia REPL",
+                "focus": True,
+                "tag": "lsp_julia_repl"
+            })
+        else:
+            self.view.window().focus_view(repl_view)
+
+class JuliaExecuteCommand(LspExecuteCommand):
+    def is_enabled(self):
+        return self.view.match_selector(0, "source.julia") and self.client_with_capability(None) is not None
